e5af6cdae5d403a6f62b534891dbc20499631309
laurapease <laurapeasetm@gmail.com>
2020-11-28 17:27:34 -0500

    Initialized Project

---
+++ final_project/main_app/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+# Create your views here.
+
+
+def home(request):
+    return HttpResponse('<h1>Hello /ᐠ｡‸｡ᐟ\ﾉ</h1>')
+
+
+def about(request):
+    return HttpResponse('<h1>About Roadies4Roadies</h1>')
+
+# ----------SIGN UP
